a37e9239e5261a351c6d49044ec76722df4ec6f2
ISTEGAL <ISTEGAL@ISTEGALPC.localdomain>
2017-04-08 22:10:43 +0200

    test

---
+++ src/de/hsMannheim/informatik/tpe/uebung1/test.java
@@ -0,0 +1,10 @@
+package de.hsMannheim.informatik.tpe.uebung1;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
