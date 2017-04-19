3da1b874713f54992537c27e6ff074484adea0ff
ISTEGAL <ISTEGAL@ISTEGALPC.fritz.box>
2017-04-19 22:34:41 +0200

    test

--- src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2/Eis.java
+++ src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2/Eis.java
@@ -1,5 +1,7 @@
 package de.hsMannheim.informatik.tpe.ss17.repo27.uebung2;
 
+import static gdi.MakeItSimple.readInt;
+
 public class Eis {
 
 	String name;
@@ -7,16 +9,63 @@ public class Eis {
 	String behaeltnis;
 	String art;
 	String[] sorten = {"Vanille","Schokolade","Straciatella","Zitron"};
-	String[] extra  = {"Sahne"," Schokostraeusel","Keks","Soﬂe"};
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	String[] extras  = {"Sahne"," Schokostraeusel","Keks","Soﬂe"};
 
+	public Eis(String name, double preis, String behaeltnis, String art, String[] sorten, String[] extras){
+		this.name = name;
+		this.preis = preis;
+		this.behaeltnis = behaeltnis;
+		this.art = art;
+		this.sorten = sorten;
+		this.extras = extras;
+		vorbereitet();
 	}
-	
-	void vorbereitet(){
 		
+	void vorbereitet(){
+		switch (name) {
+		case "Spaghettieis":
+			preis = 4.99;
+			art = "Spaghettis";
+			break;
+		case "Bananasplit":
+			preis = 4.49;
+			art = "";
+			break;
+		case "Nussbecher":
+			preis = 3.99;
+			art = "";
+			break;
+		default: 
+			preis = 0.0;
+			break;
+		};
+		
+		switch (behaeltnis) {
+		case "Waffel":
+			behaeltnis = "Waffel";
+			preis = preis+0.10;
+			break;
+		case "Becher":
+			behaeltnis = "Becher";
+			preis = preis+0.0;
+			break;
+		case "Teller":
+			behaeltnis = "Teller";
+			preis = preis+0.05;
+			break;
+		case "Schussel":
+			behaeltnis = "Schussel";
+			preis = preis+0.05;
+			break;
+		case "Glas":
+			behaeltnis = "Glas";
+			preis = preis+0.15;
+			break;
+		default: 
+			behaeltnis = "";
+			preis = 0;
+			break;
+		};
 	}
 	
 	void fuellen(){
@@ -25,6 +74,10 @@ public class Eis {
 	
 	void dekorieren(){
 		
+	}
+	
+	double gebePreisZurueck(){
+		return preis;
 	}
 
 }
--- src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2/Eisdiele.java
+++ src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2/Eisdiele.java
@@ -2,31 +2,103 @@ package de.hsMannheim.informatik.tpe.ss17.repo27.uebung2;
 
 import static gdi.MakeItSimple.*;
 
-public class Eisdiele {
-
+public class Eisdiele{
+	static String typ;
+	static String behaeltnis;
+	static String art;
+	static String[] sorten;
+	static String[] extras;
+	static Eis eis;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-	
-	public void bestellen(String typ){
+		begruessen();
+		println("=====Eiskarte=====");
+		println("Bitte geben Sie ihr Wunscheis ein!");
+		println("1.Spaghettieis");
+		println("2.Bananasplit");
+		println("3.Nussbecher");
+		typ = readLine();
+		switch (typ) {
+		case "Spaghettieis":
+			break;
+		case "Bananasplit":
+
+			break;
+		case "Nussbecher":
+			break;
+		default: 
+			entschuldigen();
+			break;
+		};
+		
+		println("Bitte wählen Sie nun Ihr Wunschbehältnis!");
+		println("Waffel");
+		println("Becher");
+		println("Teller");
+		println("Schüssel");
+		println("Glas");
+		behaeltnis = readLine();
+		
+		println("Bitte wählen Sie nun die Art von Ihrem Eis!");
+		println("Bällchen");
+		println("Bälle");
+		println("Spaghettis");
+		println("Kekse");
+		art = readLine();
+		
+		println("Was für eine Sorte wollen Sie haben?");
+		println("Vanille");
+		println("Schokolade");
+		println("Straciatella");
+		println("Zitrone");
+		//sorten;
+		
+		println("Möchten Sie noch paar extras auf Ihrem Eis?");
+		println("Sahne");
+		println("Schokosträusel");
+		println("Keks");
+		println("Soße");
+		//extras = readLine();
+		
+		bestellen(typ);
 		
 	}
+
 	
-	void begruessen(){
-		
+	public static void bestellen(String typ){
+		eis = new Eis(typ,0.0,behaeltnis,art,sorten,extras);
+		kassieren();
+	}
+
+
+	static void begruessen(){
+		println("Hallo und Herzlich Willkommen zu unserer Eisdiele!");
+		println("Wie möchten Sie ihr Eis gerne habe!");
 	}
 	
-	void kassieren(){
-		
+	static void kassieren(){
+		double preis = eis.gebePreisZurueck();
+		println("Das macht dann " + preis+"€");
+		double geld = readInt();
+		double restgeld=geld-preis;
+		if(restgeld<0){
+			println("Es fehlen noch "+(restgeld*(-1))+"€");
+
+		}
+		else{
+			println("Sie bekommen noch "+restgeld+"€");
+			verabschieden();
+		}
 	}
 	
-	void verabschieden(){
+	static void verabschieden(){
 		println("Auf Wiedersehen! Und lassen Sie sich ihr Eis schmecken!");
 	}
 	
-	void entschuldigen(){
-		
+	static void entschuldigen(){
+		println("Entschulden Sie, aber die von Ihnen ausgewählte Sorte haben wir leider nicht zur verfügung!");
 	}
 
 }
