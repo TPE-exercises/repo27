2ebaba19b7314c4dfe43881c9fbb2a7fc01c144b
ISTEGAL <ISTEGAL@ISTEGALPC.fritz.box>
2017-04-19 20:26:58 +0200

    Klassen und Methoden erstellt

---
+++ src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2/Eis.java
@@ -0,0 +1,30 @@
+package de.hsMannheim.informatik.tpe.ss17.repo27.uebung2;
+
+public class Eis {
+
+	String name;
+	double preis;
+	String behaeltnis;
+	String art;
+	String[] sorten = {"Vanille","Schokolade","Straciatella","Zitron"};
+	String[] extra  = {"Sahne"," Schokostraeusel","Keks","Soﬂe"};
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	void vorbereitet(){
+		
+	}
+	
+	void fuellen(){
+		
+	}
+	
+	void dekorieren(){
+		
+	}
+
+}
---
+++ src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2/Eisdiele.java
@@ -0,0 +1,32 @@
+package de.hsMannheim.informatik.tpe.ss17.repo27.uebung2;
+
+import static gdi.MakeItSimple.*;
+
+public class Eisdiele {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void bestellen(String typ){
+		
+	}
+	
+	void begruessen(){
+		
+	}
+	
+	void kassieren(){
+		
+	}
+	
+	void verabschieden(){
+		println("Auf Wiedersehen! Und lassen Sie sich ihr Eis schmecken!");
+	}
+	
+	void entschuldigen(){
+		
+	}
+
+}
