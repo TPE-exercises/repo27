56c16f1580477e78c4930722933eac01104f87c5
Ufuk <Ufuk@141.19.158.35>
2017-05-02 15:41:57 +0200

    teil 2 aufgabe 2 sortierverfahren

---
+++ src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2_teil2/Menue_Sortieren.java
@@ -0,0 +1,86 @@
+package de.hsMannheim.informatik.tpe.ss17.repo27.uebung2_teil2;
+
+import static gdi.MakeItSimple.*;
+
+import java.util.Arrays;
+
+public class Menue_Sortieren {
+
+	static Comparable[] sortArray;
+
+	static Sortierverfahren sv = new Sortierverfahren();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		println("Bitte wählen Sie nun aus, was für Elemente Sie in das Array einfügen wollen!");
+		println("1. Integer-Elemente");
+		println("2. String-Elemente");
+		int auswahl = readInt();
+		switch (auswahl) {
+		case 1:
+			println("Bitte geben Sie die Anzahl der Elemente hier an, die Sie eingeben wollen!");
+			int anzahl = readInt();
+			println("-------------");
+			if (anzahl == 0) {
+				println("[]");
+			} else {
+				sortArray = new Comparable[anzahl];
+				int index = 0;
+				while (sortArray[index] == null) {
+					sortArray[index] = readInt();
+					index++;
+					if (index == anzahl) {
+						break;
+					}
+				}
+				auswahl_Sortierverfahren(sortArray);
+				break;
+			}
+		case 2:
+			println("Bitte geben Sie die Anzahl der Elemente hier an, die Sie eingeben wollen!");
+			int anzahl2 = readInt();
+			println("-------------");
+			sortArray = new Comparable[anzahl2];
+			int index2 = 0;
+			String x = readLine();
+			for (int i = 0; i < sortArray.length; i++) {
+				sortArray[i] = readLine();
+			}
+			auswahl_Sortierverfahren(sortArray);
+			break;
+		default:
+			println("Auswahl nich gefunden");
+			break;
+		};
+	}
+	
+	public static void auswahl_Sortierverfahren(Comparable[] array){
+		print("Ihre eingegebenen Daten (\"Unsortiert\"): ");
+		println(Arrays.toString(sortArray));
+		println("Bitte wählen sie nun aus, welches Sortierverfahren Sie benutzen wollen!");
+		println("1. Insertion-Sort");
+		println("2. Shaker-Sort");
+		println("3. Insertion-/Shaker-Sort");
+		int auswahlS_Verfahren = readInt();
+		switch(auswahlS_Verfahren){
+		case 1:
+			print("Insertion-Sort: ");
+			sv.insertionSort(sortArray);
+			break;
+		case 2:
+			print("Shaker-Sort: ");
+			sv.shakerSort(sortArray);
+			break;
+		case 3:
+			print("Insertion-Sort: ");
+			sv.insertionSort(sortArray);
+			print("Shaker-Sort: ");
+			sv.shakerSort(sortArray);
+			break;
+		default:
+			println("Auswahl nich gefunden");
+		break;
+		};
+		
+	}
+}
--- src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2_teil2/Sortierverfahren.java
+++ src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2_teil2/Sortierverfahren.java
@@ -1,46 +1,78 @@
 package de.hsMannheim.informatik.tpe.ss17.repo27.uebung2_teil2;
 
-import static gdi.MakeItSimple.println;
-
+import static gdi.MakeItSimple.*;
 import java.util.Arrays;
-import  java.lang.*;
+import java.lang.*;
 
-
-public class Sortierverfahren implements Comparable{
+public class Sortierverfahren implements Comparable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Comparable[] integer = {10,4,1,3,2,6,7,32,11};
-		Comparable[] string = {"Birne","Apfel","Banane","Kiwi","Mango","Traube"};
-		
+		Comparable[] integer = { 10, 4, 1, 3, 2, 6, 7, 32, 11 };
+		Comparable[] string = {"Traube", "Birne", "Apfel", "Banane", "Kiwi", "Mango" };
+
+		print("Unsortiert Integer: ");
+		println(Arrays.toString(integer));
+		print("Insertion Sort: ");
 		insertionSort(integer);
+		print("Shaker Sort: ");
+		shakerSort(integer);
+		
+		println("");
+		
+		print("Unsortiert String: ");
+		println(Arrays.toString(string));
+		print("Insertion Sort: ");
+		insertionSort(string);
+		print("Shaker Sort: ");
+		shakerSort(string);
 	}
-	public static void insertionSort (Comparable[] list)
-	   {
-	      for (int index = 0; index < list.length; index++)
-	      {
-	         Comparable key = list[index];
-	         int position = index;
 
-	         //  Shift larger values to the right
-	         while (position > 0 && key.compareTo(list[position-1]) < 0)
-	         {
-	            list[position] = list[position-1];
-	            position--;
-	         }
-	            
-	         list[position] = key;
-	         println(list[position]);
+	public static void insertionSort(Comparable[] list) {
+		for (int index = 0; index < list.length; index++) {
+			Comparable key = list[index];
+			int position = index;
 
-	      }
-	      
-	   }
+			// Shift larger values to the right
+			while ((position > 0) && key.compareTo(list[position - 1]) < 0) {
+				list[position] = list[position - 1];
+				position--;
+			}
+
+			list[position] = key;
+
+		}
+		println(Arrays.toString(list));
+	}
+
+	public static void shakerSort(Comparable[] array) {
+	    for (int i = 0; i < array.length/2; i++) {
+	        boolean swapped = false;
+	        for (int j = i; j < array.length - i - 1; j++) {
+	            if (array[j].compareTo(array[j]) < array[j].compareTo(array[j+1])) {
+	                Comparable tmp = array[j];
+	                array[j] = array[j+1];
+	                array[j+1] = tmp;
+	                swapped = true;
+	            }
+	        }
+	        for (int j = array.length - 2 - i; j > i; j--) {
+	            if (array[j].compareTo(array[j]) > array[j].compareTo(array[j-1])) {
+	            	Comparable tmp = array[j];
+	                array[j] = array[j-1];
+	                array[j-1] = tmp;
+	                swapped = true;
+	            }
+	        }
+	        if(!swapped) break;
+	    }
+		println(Arrays.toString(array));
+	}
+
 	@Override
 	public int compareTo(Object o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
-
 
 }
