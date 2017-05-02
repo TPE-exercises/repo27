a92b934b3ac2a237005a2dfb15d96eec2e26725c
Ufuk <Ufuk@141.19.158.35>
2017-05-02 16:37:08 +0200

    bug fix

--- src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2_teil2/aufgabe2/Menue_Sortieren.java
+++ src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung2_teil2/aufgabe2/Menue_Sortieren.java
@@ -6,10 +6,6 @@ import java.util.Arrays;
 
 public class Menue_Sortieren {
 
-	static Comparable[] sortArray;
-
-	static Sortierverfahren sv = new Sortierverfahren();
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		println("Bitte wählen Sie nun aus, was für Elemente Sie in das Array einfügen wollen!");
@@ -20,67 +16,108 @@ public class Menue_Sortieren {
 		case 1:
 			println("Bitte geben Sie die Anzahl der Elemente hier an, die Sie eingeben wollen!");
 			int anzahl = readInt();
+			int[] arrayInt = new int[anzahl];
 			println("-------------");
 			if (anzahl == 0) {
 				println("[]");
 			} else {
-				sortArray = new Comparable[anzahl];
-				int index = 0;
-				while (sortArray[index] == null) {
-					sortArray[index] = readInt();
-					index++;
-					if (index == anzahl) {
-						break;
-					}
+				for (int index = 0; index < arrayInt.length; index++) {
+					arrayInt[index] = readInt();
+
+					 if (index == anzahl) {
+					 break;
+					 }
 				}
-				auswahl_Sortierverfahren(sortArray);
+
+				auswahl_Sortierverfahren(MyIntArray(arrayInt));
 				break;
 			}
 		case 2:
 			println("Bitte geben Sie die Anzahl der Elemente hier an, die Sie eingeben wollen!");
 			int anzahl2 = readInt();
+			String[] arrayString = new String[anzahl2];
 			println("-------------");
-			sortArray = new Comparable[anzahl2];
 			int index2 = 0;
 			String x = readLine();
-			for (int i = 0; i < sortArray.length; i++) {
-				sortArray[i] = readLine();
+			for (int i = 0; i < arrayString.length; i++) {
+				arrayString[i] = readLine();
 			}
-			auswahl_Sortierverfahren(sortArray);
+			auswahl_Sortierverfahren(MyString(arrayString));
 			break;
 		default:
 			println("Auswahl nich gefunden");
 			break;
 		};
 	}
+
+	private static MyInt[] MyIntArray(int[] array) {
+
+		MyInt[] myInt = new MyInt[array.length];
+
+		for (int i = 0; i < array.length; ++i) {
+			myInt[i] = new MyInt(array[i]);
+		}
+
+		return myInt;
+	}
 	
-	public static void auswahl_Sortierverfahren(Comparable[] array){
-		print("Ihre eingegebenen Daten (\"Unsortiert\"): ");
-		println(Arrays.toString(sortArray));
+	private static MyString[] MyString(String[] array) {
+
+		MyString[] myString = new MyString[array.length];
+
+		for (int i = 0; i < array.length; ++i) {
+			myString[i] = new MyString(array[i]);
+		}
+
+		return myString;
+	}
+
+	public static void auswahl_Sortierverfahren(Comparable[] array) {
+
 		println("Bitte wählen sie nun aus, welches Sortierverfahren Sie benutzen wollen!");
 		println("1. Insertion-Sort");
 		println("2. Shaker-Sort");
 		println("3. Insertion-/Shaker-Sort");
 		int auswahlS_Verfahren = readInt();
-		switch(auswahlS_Verfahren){
+		switch (auswahlS_Verfahren) {
 		case 1:
-			print("Insertion-Sort: ");
-			sv.insertionSort(sortArray);
+			sortArray(1, array);
 			break;
 		case 2:
-			print("Shaker-Sort: ");
-			sv.shakerSort(sortArray);
+			sortArray(2, array);
 			break;
 		case 3:
-			print("Insertion-Sort: ");
-			sv.insertionSort(sortArray);
-			print("Shaker-Sort: ");
-			sv.shakerSort(sortArray);
+			sortArray(3, array);
 			break;
 		default:
 			println("Auswahl nich gefunden");
-		break;
+			break;
 		};
-		
+
+	}
+
+	private static void sortArray(int sortAlgorithm, Comparable[] array) {
+
+		print("Ihre eingegebenen Daten (\"Unsortiert\"): ");
+		println(Arrays.toString(array));
+		switch (sortAlgorithm) {
+		case 1:
+			print("Insertion-Sort: ");
+			Sortierverfahren.insertionSort(array);
+			break;
+		case 2:
+			print("Shaker-Sort: ");
+			Sortierverfahren.shakerSort(array);
+			break;
+		case 3:
+			print("Insertion-Sort: ");
+			Sortierverfahren.insertionSort(array);
+			print("Shaker-Sort: ");
+			Sortierverfahren.shakerSort(array);
+			break;
+		default:
+			break;
+		};
+
 	}
 }
