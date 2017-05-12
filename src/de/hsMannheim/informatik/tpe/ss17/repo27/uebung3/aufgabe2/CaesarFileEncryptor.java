8b0f388021461a341f6f23d204c114f20901856f
Ufuk <Ufuk@141.19.152.66>
2017-05-12 17:30:28 +0200

    .

---
+++ src/de/hsMannheim/informatik/tpe/ss17/repo27/uebung3/aufgabe2/CaesarFileEncryptor.java
@@ -0,0 +1,24 @@
+package de.hsMannheim.informatik.tpe.ss17.repo27.uebung3.aufgabe2;
+
+import java.io.File;
+
+public class CaesarFileEncryptor implements IFileEncryptor{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public File encrypt(File sourceDirectory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public File decrypt(File sourceDirectory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
